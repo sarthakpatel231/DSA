d86dd1778a04dcc41abbe3eecae7c1743209e9c3
Sarthak Patel <sarthakpatel231@gmail.com>
2024-05-31 01:23:48 +0530

    BubbleSort Added

--- Sorting/src/Bubble.java
+++ Sorting/src/Bubble.java
@@ -1,5 +1,28 @@
 public class Bubble {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            // If no two elements were swapped by inner loop, then break
+            if (!swapped) break;
+        }
+    }
     public static void main(String[] args) {
-
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+        bubbleSort(array);
+        System.out.println("Sorted array:");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
     }
 }
